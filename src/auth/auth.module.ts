e7cc50d9f654bb58013d968ab32863ba50620c83
D34ultCoder <sabitsdq@gmail.com>
2025-07-31 11:46:49 +0100

    rebuild the project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,42 +1,11 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-// import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import appConfig from '../helpers/auth.config';
-// import appConfig from '../../../config/auth.config';
-// import { User } from '../user/entities/user.entity';
-// import UserService from '../user/user.service';
-// import RegistrationController from './auth.controller';
-// import AuthenticationService from './auth.service';
-// import { GoogleAuthService } from './google-auth.service';
-// import { FacebookStrategy } from './strategies/facebook.strategy';
-// import { GoogleStrategy } from './strategies/google.strategy';
-// import { EmailService } from '../mailer/mailer.service';
-// import { WalletModule } from '../wallet/wallet.module';
+import { JwtGlobalModule } from 'src/utils/jwt-global.module';
 
 @Module({
-  // controllers: [RegistrationController],
-  providers: [
-    // AuthenticationService,
-    Repository,
-    // UserService,
-    // GoogleStrategy,
-    // GoogleAuthService,
-    // FacebookStrategy,
-    // EmailService,
-  ],
-  imports: [
-    TypeOrmModule.forFeature(),
-    // TypeOrmModule.forFeature([User]),
-    // PassportModule,
-    // WalletModule
-    JwtModule.register({
-      global: true,
-      secret: appConfig().jwtSecret,
-      signOptions: { expiresIn: `${appConfig().jwtExpiry}s` },
-    }),
-  ],
+  providers: [Repository],
+  imports: [TypeOrmModule.forFeature([]), JwtGlobalModule],
   exports: [],
 })
 export class AuthModule {}
